2a7946c2263723eb9fa794c9ee6e5122969c840f
P4CKt <omurcanuslu@outlook.com>
2023-03-06 14:48:51 +0300

    MVP'ye Ulaşıldı

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
-
+import { CartContext } from "./contexts/ProductContext";
 // Bileşenler
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
 
 function App() {
   const [products, setProducts] = useState(data);
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    // verilen itemi sepete ekleyin
+    !cart.find((items) => items.id === item.id)
+      ? setCart([...cart, item])
+      : console.log("Ürün Sepette VAr");
   };
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Navigation />
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+        {/* Routelar */}
+        <main className="content">
+          <ProductContext.Provider value={{ products, addItem }}>
+            <Route exact path="/">
+              <Products />
+            </Route>
+          </ProductContext.Provider>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </main>
+      </CartContext.Provider>
     </div>
   );
 }
